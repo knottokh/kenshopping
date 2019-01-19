468ee76bcbe57c806eedf9cbda30d001cea8953a
Knotto <one2treego@hotmail.com>
2019-01-19 12:36:21 +0000

    fix best seller 1

--- app/overrides/spree/products/_show.rb
+++ app/overrides/spree/products/_show.rb
@@ -6,7 +6,7 @@ Deface::Override.new(
       <% if @product.recommend %>
          <p class="icon-reccommend">icon reccommend</p>
      <% end %>
-     <% show_seller_check = Spree::Product.best_sellers %>
+     <% show_seller_check = (Spree::Product.best_sellers rescue nil) %>
     <% unless show_seller_check.present? %>
          <% unless show_seller_check.select { |pt| pt.id == @product.id }.empty? %>
             <p class="icon-base-sellers">icon base sellers</p>
