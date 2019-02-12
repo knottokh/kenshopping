fbd5c9394b97c5b7dceeeb91f46def5920f69ce2
Knotto <one2treego@hotmail.com>
2019-02-12 18:36:45 +0000

    edit email template in spree-theme

--- app/overrides/spree/order_mailer/confirm_email.rb
+++ app/overrides/spree/order_mailer/confirm_email.rb
@@ -2,5 +2,31 @@ Deface::Override.new(
   virtual_path: 'spree/order_mailer/confirm_email',
   name: 'order_mailer confirm_email',
   replace: "table.header",
-  partial: 'spree/mail_template/confirm_email'
+  text: %q{
+      <%= render file: "/#{Dir.getwd }/public/vinsol_spree_themes/current/mailer/confirm_email" %>
+  }
+)
+Deface::Override.new(
+  virtual_path: 'spree/order_mailer/cancel_email',
+  name: 'order_mailer cancel_email',
+  replace: "table.header",
+  text: %q{
+      <%= render file: "/#{Dir.getwd }/public/vinsol_spree_themes/current/mailer/cancel_email" %>
+  }
+)
+Deface::Override.new(
+  virtual_path: 'spree/reimbursement_mailer/reimbursement_email',
+  name: 'reimbursement_mailer reimbursement_email',
+  replace: "table.header",
+  text: %q{
+      <%= render file: "/#{Dir.getwd }/public/vinsol_spree_themes/current/mailer/reimbursement_email" %>
+  }
+)
+Deface::Override.new(
+  virtual_path: 'spree/shipment_mailer/shipped_email',
+  name: 'shipment_mailer confirm_email',
+  replace: "table.header",
+  text: %q{
+      <%= render file: "/#{Dir.getwd }/public/vinsol_spree_themes/current/mailer/shipped_email" %>
+  }
 )
